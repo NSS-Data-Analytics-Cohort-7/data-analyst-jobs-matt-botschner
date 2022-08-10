7e95eaa51ce9aaa7eed2a79c6fe16099ee09cca0
Matt Botschner <matt.botschner@gmail.com>
2022-08-09 19:46:46 -0500

    complete

--- scripts/scripts.sql
+++ scripts/scripts.sql
@@ -86,30 +86,26 @@ ORDER BY avg_rating DESC;*/
     How many different job titles are there?*/
 /*SELECT DISTINCT title
 FROM data_analyst_jobs
-WHERE title Like '%Analyst%';*/
---11)754
+WHERE title ILike '%Analyst%';*/
+--11)774
 
 /*12)How many different job titles do not contain either 
     the word ‘Analyst’ or the word ‘Analytics’? 
     What word do these positions have in common?*/
 /*SELECT title
 FROM data_analyst_jobs
-WHERE title NOT LIKE '%Analyst%' 
-AND title NOT LIKE '%Analytics%'
-AND title NOT LIKE '%ANALYST%'
-AND title NOT LIKE '%analyst%'
-AND title NOT LIKE '%ANALYTICS%'
-AND title NOT LIKE '%analytics%';*/
+WHERE title NOT ILIKE '%Analyst%' 
+AND title NOT ILIKE '%Analytics%';*/
 --12) 4, Tableau
 
-/*Bonus) You want to understand which jobs requiring SQL are hard to fill. 
+/* Bonus) You want to understand which jobs requiring SQL are hard to fill. 
     Find the number of jobs by industry (domain) that require SQL and have been 
     posted longer than 3 weeks.
         Disregard any postings where the domain is NULL.
         Order your results so that the domain with the greatest number of 
         hard to fill jobs is at the top.
         Which three industries are in the top 4 on this list? 
-        How many jobs have been listed for more than 3 weeks for each of the top 4?
+        How many jobs have been listed for more than 3 weeks for each of the top 4?*/
 /*SELECT title, days_since_posting, domain 
 FROM data_analyst_jobs
 WHERE days_since_posting > 21
@@ -121,8 +117,8 @@ ORDER BY days_since_posting desc, domain;*/
 --Auto 5
 --Banks and Financial Services 39
 
-
-
+SELECT *
+FROM data_analyst_jobs;
 
 
 
